ce1d987ca6ef60025669fbd58164da41d0d856ae
kenhsu <kenhsu@fashionguide.com.tw>
2016-01-28 18:37:46 +0800

    add about service contact

--- app/Http/Controllers/Blog.php
+++ app/Http/Controllers/Blog.php
@@ -6,15 +6,15 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Article;
+use Illuminate\Support\Facades\Route;
 
 class Blog extends Controller
 {
-
+    
     public function showArticle(Request $request, $id)
     {
         $article= (new Article())->geSingletArticle($id);
         return view('blog._blog', ["article"=>$article]);
     }
-    
 
 }
